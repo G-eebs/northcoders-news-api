b2785f5589fb0ccaabdf8cd743c4aa6ad01476b5
George B <george.bartram29@gmail.com>
2024-04-16 09:35:44 +0100

    amend error handling

--- app/errors/index.js
+++ app/errors/index.js
@@ -13,7 +13,7 @@ exports.handleCustomErrors = (err, req, res, next) => {
 exports.handlePsqlErrors = (err, req, res, next) => {
 	if (err.code === "22P02") {
 		res.status(400).send({ msg: "Invalid Input" });
-	}
+	} else next(err);
 };
 
 exports.handleServerErrors = (err, req, res, next) => {
